331e3f509b9c45847fe3ad4fcccec11c58a15c8f
Abhijit25Mishra <abhijitmishra20bcs94@iiitkottayam.ac.in>
2022-02-25 11:33:43 +0530

    creating new body component

--- Enlace-main/src/components/Vertical.jsx
+++ Enlace-main/src/components/Vertical.jsx
@@ -1,10 +1,12 @@
 import styles from "./vertical.module.css";
 import Nav from "./Navbar.jsx";
+import Body from "./body.jsx";
 
 const Vertical = () => {
     return (
         <div className = {styles.VerticalWrapper}>
             <Nav/>
+            <Body/>
             <div className = {styles.page}>Vertical - 1</div>
             <div className = {styles.page}>Vertical - 2</div>
             <div className = {styles.page}>Vertical - 3</div>
