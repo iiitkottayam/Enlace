26674adc64fd5346aa6a5aaa1124a22e4b307b1b
Abhijit25Mishra <abhijitmishra20bcs94@iiitkottayam.ac.in>
2022-02-25 22:37:59 +0530

    nav bar changes

--- Enlace-main/src/components/Navbar.jsx
+++ Enlace-main/src/components/Navbar.jsx
@@ -2,16 +2,17 @@ const Navbar = () => {
   return (
     <nav className=" container flex sticky top-0 align-baseline justify-between px-4 py-3  mx-auto border-solid border-2 border-border-nav[0.69] bg-bg-nav/[0.6] rounded-full">
       <div className="text-white">
-        <h3 className="text-2xl font-medium text-500 ">
+        <h3 className="text-4xl font-medium text-700 ">
           {/* <img  className="h-12 w-12 float-left pr-2" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="logo" />  */}
           Enlace
         </h3>
       </div>
-      <div class="hidden space-x-12 text-white lg:flex">
+      <div class="hidden space-x-16 text-500 text-white  lg:flex items-center">
         <a href="">Events</a>
         <a href="">Sponsors</a>
         <a href="">FAQ</a>
-        <a href="">Programs</a>
+        <a href="">Performace</a>
+        <a href="">Workshops</a>
         <a href="">Contact</a>
       </div>
       <div class="flex lg:hidden">
