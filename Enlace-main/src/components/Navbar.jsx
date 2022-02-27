f9b34d61a9aeb6e46cb2631c309d0ebe7c883b15
Abhijit25Mishra <abhijitmishra20bcs94@iiitkottayam.ac.in>
2022-02-27 15:00:28 +0530

    responseiveness

--- Enlace-main/src/components/Navbar.jsx
+++ Enlace-main/src/components/Navbar.jsx
@@ -1,21 +1,21 @@
 const Navbar = () => {
   return (
-    <nav className="hidden lg:flex box-border container  absolute top-0 left-0 right-0  justify-between px-12 py-5 mx-auto border-solid border-2 border-border-nav[0.69] bg-bg-nav/[0.6] rounded-full">
+    <nav className="hidden md:flex box-border container  absolute top-0 left-0 right-0  justify-between px-4 py-5 mx-auto border-solid border-2 border-border-nav[0.69] bg-bg-nav/[0.6] rounded-full ">
       <div className="text-white">
         <h3 className="text-4xl font-medium text-700 ">
           {/* <img  className="h-12 w-12 float-left pr-2" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="logo" />  */}
           Enlace
         </h3>
       </div>
-      <div className="hidden space-x-16 text-500 text-white  lg:flex items-center">
+      <div className="hidden space-x-8 text-500 text-white lg:space-x-16  md:flex items-center">
         <a href="">Events</a>
         <a href="">Sponsors</a>
         <a href="">FAQ</a>
         <a href="">Performace</a>
         <a href="">Workshops</a>
         <a href="">Contact</a>
-      </div>
-      <div className="flex lg:hidden">
+      </div>  
+      <div className="flex md:hidden">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="w-6 h-6"
--- Enlace-main/src/components/Vertical.jsx
+++ Enlace-main/src/components/Vertical.jsx
@@ -8,22 +8,21 @@ const Vertical = () => {
       <Nav />
       {/* <Body /> */}
       <div className={styles.page}>
-        <div className="mx-auto w-11/12 h-4/6 flex flex-col flex-nowrap justify-around items-start">
-          <h2 className="text-5xl text-white ">Welcome to Enlace!</h2>
-          <p className="w-6/12 text-white text-2xl ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit veniam fugit omnis pariatur porro, ipsum molestias velit aperiam suscipit accusamus quos corporis, reiciendis tempore dicta?</p>
+        <div className="w-11/12 h-4/6 mx-auto flex flex-col flex-nowrap justify-around items-center pt-16 lg:pt-32 lg:items-start  md:h-3/6 md:pt-16 md:items-start">
+          <h2 className="text-4xl text-white lg:text-5xl">Welcome to Enlace!</h2>
+          <p className="w-11/12 lg:w-6/12 text-white text-2xl py-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit veniam fugit omnis pariatur porro, ipsum molestias velit aperiam suscipit accusamus quos corporis, reiciendis tempore dicta?</p>
           <a href="#" className="inline-block rounded bg-[#FFF6D8]/90  px-12 py-2 border-2 border-[#FF3D3D]">Register!</a>
         </div>
         
       </div>
       <div className={styles.page}>
-        <div className="mx-auto w-11/12 h-full flex flex-col flex-nowrap justify-center items-start">
-        <h2 className="text-5xl text-white pb-4 ">Welcome to Enlace!</h2>
-          <p className="w-6/12 text-white text-2xl py-16">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit veniam fugit omnis pariatur porro, ipsum molestias velit aperiam suscipit accusamus quos corporis, reiciendis tempore dicta?</p>
-          <p className="w-6/12 text-white text-2xl ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur vitae, maxime minima debitis autem similique culpa corporis reiciendis?</p>
+        <div className="mx-auto w-11/12 h-full flex flex-col flex-nowrap justify-center items-center  lg:items-start md:h-4/6 md:items-start">
+        <h2 className="text-4xl text-white lg:text-5xl pb-4">Welcome to Enlace!</h2>
+          <p className="w-11/12 lg:w-6/12 text-white text-2xl py-16">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit veniam fugit omnis pariatur porro, ipsum molestias velit aperiam suscipit accusamus quos corporis, reiciendis tempore dicta?</p>
+          <p className="w-11/12 lg:w-6/12 text-white text-2xl ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur vitae, maxime minima debitis autem similique culpa corporis reiciendis?</p>
         </div>
       </div>
       <div className={styles.page}> <div className="flex justify-center items-center h-full w-full"><img src={dates} alt="dates" /></div></div>
-       
     </div>
   );
 };
