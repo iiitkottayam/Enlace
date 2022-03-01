76a694910c4e32d5d0280771407550ef1918c627
Abhijit25Mishra <abhijitmishra20bcs94@iiitkottayam.ac.in>
2022-03-02 01:08:07 +0530

    Resister button added as a component

---
+++ Enlace-main/src/components/RegisterButton.jsx
@@ -0,0 +1,8 @@
+
+const RegisterButton = () => {
+    return (
+        <a href="#" className="inline-block rounded bg-[#FFF6D8]/90  px-12 py-2 border-2 border-[#FF3D3D]">Register!</a>
+    );
+};
+
+export default RegisterButton;
