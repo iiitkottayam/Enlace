abe0870558bce5b8c66780be4dbef307c8ce5555
Abhijit25Mishra <abhijitmishra20bcs94@iiitkottayam.ac.in>
2022-03-04 18:05:24 +0530

    responsive -5

--- Enlace-main/src/components/Boxes/FAQbox.jsx
+++ Enlace-main/src/components/Boxes/FAQbox.jsx
@@ -1,11 +1,11 @@
 const FAQbox = ({ question, answer }) => {
   return (
-    <div className="m-5 p-0.5 lg:w-96 lg:h-40 bg-gradient-to-r from-gradient-start/60 to-gradient-stop/60 rounded-xl box-border">
-      <div className="p-5 w-full h-full bg-white/60 rounded-xl">
-        <p className="mb-2 font-poppins font-medium lg:text-xl text-black">
+    <div className="m-5 p-0.5 lg:w-96 lg:h-40 w-96 h-44 bg-gradient-to-r from-gradient-start/60 to-gradient-stop/60 rounded-xl box-border">
+      <div className=" p-1 lg:p-5 w-full h-full bg-white/60 rounded-xl">
+        <p className="mb-2 font-poppins text-1xl font-medium lg:text-xl text-black">
           {question}
         </p>
-        <p className="font-poppins lg:text-sm text-black">{answer}</p>
+        <p className="font-poppins text-1xl lg:text-sm text-black">{answer}</p>
       </div>
     </div>
   );
