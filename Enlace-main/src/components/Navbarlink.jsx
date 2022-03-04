dd6a1aa158c4aae9b7bb223c7029ee248cba694d
freak-10 <kotikalapudisumanth@gmail.com>
2022-03-04 14:21:56 +0530

    Updated Navbar Links

--- Enlace-main/src/components/Navbarlink.jsx
+++ Enlace-main/src/components/Navbarlink.jsx
@@ -1,8 +1,9 @@
-import { Link } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
 
 const Navbarlink = ({ title, route }) => {
   return (
     <Link
+      smooth
       to={route}
       className="relative hover:after:scale-x-100 after:absolute after:left-0 after:right-0 after:-bottom-1 after:h-[2px] after:bg-white after:scale-x-0 after:origin-center after:transition-transform"
     >
