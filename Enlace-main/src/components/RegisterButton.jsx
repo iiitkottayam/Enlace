11aad8b07c351c37b9de317ad02d3e02615dacde
GetPsyched6 <roshin006@gmail.com>
2022-03-04 18:39:53 +0400

    Vertical fonts and facets Reimagined

--- Enlace-main/src/components/RegisterButton.jsx
+++ Enlace-main/src/components/RegisterButton.jsx
@@ -1,7 +1,7 @@
 
 const RegisterButton = () => {
     return (
-        <a href="#" className="inline-block rounded-xl bg-[#FFF6D8]/90  px-12 pt-2 pb-3 border-[3px] border-[#FF3D3D] font-semibold text-xl">Register!</a>
+        <a href="#" className="inline-block rounded-xl bg-[#FFF6D8]/90  px-12 py-3 border-[3px] border-[#FF3D3D] font-semibold text-xl font-poppins">Register!</a>
     );
 };
 
