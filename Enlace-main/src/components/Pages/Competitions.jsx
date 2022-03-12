6e1dd41fba35557e378c3bd3baf10872bac433f4
Deep Gandhi <97520292+DeF0017@users.noreply.github.com>
2022-03-13 00:06:43 +0530

    Added import and image property of event posters

--- Enlace-main/src/components/Pages/Competitions.jsx
+++ Enlace-main/src/components/Pages/Competitions.jsx
@@ -2,6 +2,17 @@ import Nav from "../Navbar.jsx";
 import Eventteaser from "../Boxes/Eventteaser.jsx";
 import Footer from "../Footer.jsx";
 import examplelogo from "../../assets/Enlace-logo-name.png";
+import FakeItTillYouMakeIt from "../../assets/fake_it_till_you_make_it.jpg";
+import FindtheFiend from "../../assets/1080x1080-Find_the_Fiend_Instagram.png";
+import DancingWithTheDevil from "../../assets/DancingWithTheDevil_Instagram.png";
+import PulseAndTheBeat from "../../assets/PulseAndTheBeat_Instagram.png";
+import JokesOnTheHouse from "../../assets/Jokes_on_the_house_INSTAGRAM.png";
+import StringsAndKeys from "../../assets/StringsAndKeys_Instagram.jpg";
+import BeyondTheHarmony from "../../assets/BeyondTheHarmony_Instagram.png";
+import DevilAdvocate from "../../assets/Devil_s advocate instagram.png";
+import Blizzart from "../../assets/BLIZZART_Instagram.png";
+import BattleOfBands from "../../assets/Battle Of Bands Instagrm.jpg";
+import MxEnlace from "../../assets/MxEnlace_Instagram.png";
 
 const Competitions = () => {
   return (
@@ -20,42 +31,59 @@ const Competitions = () => {
             />
             <Eventteaser
               title="Fake It Till You Make It"
+              image={FakeItTillYouMakeIt}
               path="/competitions/fake-it-till-you-make-it"
             />
             <Eventteaser
               title="Find The Fiend"
+              image={FindtheFiend}
               path="/competitions/find-the-fiend"
             />
             <Eventteaser
               title="Dancing With The Devil"
+              image={DancingWithTheDevil}
               path="/competitions/dancing-with-the-devil"
             />
             <Eventteaser
               title="Pulse And The Beat"
+              image={PulseAndTheBeat}
               path="/competitions/pulse-and-the-beat"
             />
             <Eventteaser
               title="Jokes On The House"
+              image={JokesOnTheHouse}
               path="/competitions/jokes-on-the-house"
             />
             <Eventteaser
               title="Strings And Keys"
+              image={StringsAndKeys}
               path="/competitions/strings-and-keys"
             />
             <Eventteaser
               title="Beyond The Harmony"
+              image={BeyondTheHarmony}
               path="/competitions/beyond-the-harmony"
             />
             <Eventteaser
               title="Devil's Advocate"
+              image={DevilAdvocate}
               path="/competitions/devils-advocate"
             />
-            <Eventteaser title="Blizzart" path="/competitions/blizzart" />
+            <Eventteaser 
+              title="Blizzart" 
+              image={Blizzart}
+              path="/competitions/blizzart"
+            />
             <Eventteaser
               title="Battle Of Bands"
+              image={BattleOfBands}
               path="/competitions/battle-of-the-bands"
             />
-            <Eventteaser title="Mx. Enlace" path="/competitions/mx-enlace" />
+            <Eventteaser 
+              title="Mx. Enlace"
+              image={MxEnlace} 
+              path="/competitions/mx-enlace"
+             />
           </div>
         </div>
         <div className="pt-48 lg:pt-36">
