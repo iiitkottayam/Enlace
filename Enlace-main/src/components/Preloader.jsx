f23d713c27a60fc5e248222dc44ae0e9f09aa581
freak-10 <kotikalapudisumanth@gmail.com>
2022-03-13 10:55:49 +0530

    updated preloader

--- Enlace-main/src/components/Preloader.jsx
+++ Enlace-main/src/components/Preloader.jsx
@@ -1,4 +1,4 @@
-import image from "../assets/preloaderimage.png";
+import image from "../assets/logo.png";
 import { useRef, useEffect } from "react";
 import anime from "animejs";
 
@@ -41,14 +41,14 @@ const Preloader = () => {
     <div
       ref={preloaderRef}
       onLoad={() => animationRef.current.restart()}
-      className="hidden image absolute z-50 top-0 bottom-0 left-0 right-0 pt-[150px] font-reemkufi text-4xl bg-white"
+      className="hidden image bg-gradient-to-br from-[#FFFFE5] to-[#FFD6B3] absolute z-50 top-0 bottom-0 left-0 right-0 pt-[150px] font-reemkufi space-y-5 text-4xl bg-white"
     >
       <img
         src={image}
         className="mx-auto w-[200px] h-auto"
         alt="rotating enlace logo"
       ></img>
-      <h1 className="text-center">Enlace</h1>
+      <h1 className="text-center">ENLACE</h1>
     </div>
   );
 };
