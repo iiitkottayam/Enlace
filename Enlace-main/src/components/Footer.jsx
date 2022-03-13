f5676a2d06a5edfa460938837bff64429bcecf7b
freak-10 <kotikalapudisumanth@gmail.com>
2022-03-13 12:39:30 +0530

    added hover effect to footer

--- Enlace-main/src/components/Footer.jsx
+++ Enlace-main/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/wildbeatslogo.png";
 
 const Footer = () => {
   return (
-    <div className="absolute bottom-0 bg-gradient-to-b from-[#2D4769]/0 to-[#000000]/100 text-white w-full">
+    <div className="absolute bottom-0 bg-gradient-to-b from-[#2D4769]/0 to-[#000000]/100 text-white lg:text-[#ffffffb0] w-full">
       <div className="flex flex-col md:flex-row flex-nowrap justify-between items-center">
         <img src={logo} alt="wildbeats logo" className="w-64 mx-20"></img>
         <div className="mt-5 flex flex-row flex-nowrap justify-around items-center text-base lg:text-xl mx-auto lg:mx-20">
@@ -10,17 +10,17 @@ const Footer = () => {
             href="https://www.youtube.com/channel/UCTXmRiVLxOiu214yRVfNdKQ"
             target="_blank"
           >
-            <i className="fa-brands fa-youtube mx-4"></i>
+            <i className="fa-brands fa-youtube mx-4 transition-transform lg:hover:text-white hover:scale-150"></i>
           </a>
           <a href="mailto:wildbeats@iiitkottayam.ac.in" target="_blank">
-            <i className="fa-solid fa-envelope mx-4"></i>
+            <i className="fa-solid fa-envelope mx-4 transition-transform lg:hover:text-white hover:scale-150"></i>
           </a>
           <a href="https://www.instagram.com/enlace_iiitk/" target="_blank">
-            <i className="fa-brands fa-instagram mx-4"></i>
+            <i className="fa-brands fa-instagram mx-4 transition-transform lg:hover:text-white hover:scale-150"></i>
           </a>
         </div>
       </div>
-      <p className="font-montserrat text-center py-6 text-base">
+      <p className="font-montserrat text-white text-center py-6 text-base">
         Made with <i className="fa-solid fa-heart text-red-600"> </i> by
         Wildbeats
       </p>
