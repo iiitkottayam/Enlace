6165c6a48c1c7181b51a1c967622ad15719d6a6f
freak-10 <kotikalapudisumanth@gmail.com>
2022-03-15 19:41:16 +0530

    updated for enlace nite and bug fixes

--- Enlace-main/src/components/Boxes/EnlaceNiteBanner.jsx
+++ Enlace-main/src/components/Boxes/EnlaceNiteBanner.jsx
@@ -1,48 +1,48 @@
 import { HashLink as Link } from "react-router-hash-link";
 
 const urls = [
-	{
-		name: "lg",
-		url: "https://res.cloudinary.com/roshin/image/upload/v1647343340/Events/Workshops/ENLACENITE_Web_amevlo.png",
-	},
-	{
-		name: "md",
-		url: "https://res.cloudinary.com/roshin/image/upload/v1647345992/Events/Workshops/ENLACENITE_Mobile_lt658z.png",
-	},
-	{
-		name: "sm",
-		url: "https://res.cloudinary.com/roshin/image/upload/v1647343340/Events/Workshops/ENLACENITE_instagram_ttw9ty.png",
-	},
+  {
+    name: "lg",
+    url: "https://res.cloudinary.com/roshin/image/upload/v1647343340/Events/Workshops/ENLACENITE_Web_amevlo.png",
+  },
+  {
+    name: "md",
+    url: "https://res.cloudinary.com/roshin/image/upload/v1647345992/Events/Workshops/ENLACENITE_Mobile_lt658z.png",
+  },
+  {
+    name: "sm",
+    url: "https://res.cloudinary.com/roshin/image/upload/v1647343340/Events/Workshops/ENLACENITE_instagram_ttw9ty.png",
+  },
 ];
 
 const EnlaceNiteBanner = ({ path }) => {
-	return (
-		<Link to={path + "#"}>
-			<div className="w-10/12 h-[353px] bg-white/50 rounded-3xl mx-auto my-10">
-				<img
-					src={urls[0].url}
-					alt="nite_banner"
-					className="h-full w-full rounded-3xl hidden lg:block"
-				></img>
-				<img
-					src={urls[1].url}
-					alt="nite_banner"
-					className="h-full w-full rounded-3xl hidden sm:block lg:hidden"
-				></img>
-				<img
-					src={urls[2].url}
-					alt="nite_banner"
-					className="h-full w-full rounded-3xl sm:hidden"
-				></img>
-			</div>
-		</Link>
-	);
+  return (
+    <Link to="/events/enlace-nite">
+      <div className="w-[300px] sm:w-[550px] lg:w-[900px] xl:w-[1200px] h-auto bg-white/50 rounded-3xl mx-auto my-10">
+        <img
+          src={urls[0].url}
+          alt="nite_banner"
+          className="h-full w-full rounded-3xl hidden lg:block"
+        ></img>
+        <img
+          src={urls[1].url}
+          alt="nite_banner"
+          className="h-full w-full rounded-3xl hidden sm:block lg:hidden"
+        ></img>
+        <img
+          src={urls[2].url}
+          alt="nite_banner"
+          className="h-full w-full rounded-3xl sm:hidden"
+        ></img>
+      </div>
+    </Link>
+  );
 };
 
 EnlaceNiteBanner.defaultProps = {
-	title: "Enlace Nite",
-	image: urls[0].url,
-	path: "/example",
+  title: "Enlace Nite",
+  image: urls[0].url,
+  path: "/example",
 };
 
 export default EnlaceNiteBanner;
--- Enlace-main/src/components/Boxes/Eventteaser.jsx
+++ Enlace-main/src/components/Boxes/Eventteaser.jsx
@@ -1,34 +1,35 @@
 import { HashLink as Link } from "react-router-hash-link";
 
 const urls = [
-	{
-		name: "placeholder",
-		url: "https://res.cloudinary.com/roshin/image/upload/v1647279775/Events/jokesonthehouse_yc3cqu.png",
-	},
+  {
+    name: "placeholder",
+    url: "https://res.cloudinary.com/roshin/image/upload/v1647279775/Events/jokesonthehouse_yc3cqu.png",
+  },
 ];
 
 const Eventteaser = ({ title, image, path }) => {
-	return (
-		<Link
-
-			to={path + "#"}
-			data-aos="zoom-out"
-			className="m-10 p-5 bg-gradient-to-r from-gradient-start to-gradient-stop rounded-xl flex flex-col flex-nowrap justify-center items-center w-80 h-full"
-		>
-			<img
-				src={image}
-				alt="event poster"
-				className="w-64 h-64 rounded-lg mb-5"
-			></img>
-			<p className="font-poppins font-medium text-xl text-center">{title}</p>
-		</Link>
-	);
+  return (
+    <Link
+      to={path + "#"}
+      data-aos="zoom-out"
+      className="m-10 p-2 bg-gradient-to-r from-gradient-start to-gradient-stop rounded-xl flex flex-col flex-nowrap justify-center items-center h-full"
+    >
+      <img
+        src={image}
+        alt="event poster"
+        className="w-64 h-64 rounded-lg mb-5"
+      ></img>
+      <p className="w-64 font-poppins font-medium text-xl text-center">
+        {title}
+      </p>
+    </Link>
+  );
 };
 
 Eventteaser.defaultProps = {
-	title: "Jokes on the house",
-	image: urls[0].url,
-	path: "/example",
+  title: "Jokes on the house",
+  image: urls[0].url,
+  path: "/example",
 };
 
 export default Eventteaser;
